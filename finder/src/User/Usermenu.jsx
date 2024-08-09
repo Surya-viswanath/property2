8b890663300fb608af9ae449f4dc81a3ab39385e
Surya-viswanath <121759302+Surya-viswanath@users.noreply.github.com>
2024-08-09 22:43:31 +0530

    Update Usermenu.jsx

--- finder/src/User/Usermenu.jsx
+++ finder/src/User/Usermenu.jsx
@@ -36,7 +36,7 @@ const [dropdownOpen, setDropDown] = useState(false);
     const pemail=email;
     event.preventDefault();
     try {
-      const display = await axios.post('http://localhost:4008/login', { email, password });
+      const display = await axios.post('https://property1-uoj3.onrender.com/login', { email, password });
       console.log(display.data);
       const userData = display.data;
       localStorage.setItem('user', JSON.stringify(userData));
@@ -50,7 +50,7 @@ const [dropdownOpen, setDropDown] = useState(false);
   useEffect(() => {
     const handleItems = async () => {
       try {
-        const response = await axios.get('http://localhost:4008/getpro');
+        const response = await axios.get('https://property1-uoj3.onrender.com/getpro');
         setfirst(response.data);
       } catch (error) {
         console.error('Error fetching data:', error);
